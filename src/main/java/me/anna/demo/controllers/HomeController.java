b197445af65f2127daa457ed166b635027b98574
aavolkova <avolkova@montgomerycollege.edu>
2017-08-23 18:15:46 -0400

    The Walkthrough: Basic Security, Adding Custom Login page

---
+++ src/main/java/me/anna/demo/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package me.anna.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+
+}
